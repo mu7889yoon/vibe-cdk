710eb90faad6c77fd856656e80f0113bebb8f3cc
seeds-ynakamura <y.nakamura@seeds-std.co.jp>
2025-07-12 14:35:06 +0900

    Chaos100StackにAPI Gatewayとフロントエンド静的ホスティングの構成を追加し、新しいアウトプットとしてAPI GatewayのURL、UI Handler LambdaのARN、ウェブサイトのURLを出力する設定を追加しました。

--- cdk/lib/chaos-100-stack.ts
+++ cdk/lib/chaos-100-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { StepFunctionScenarioGen } from './constructs/stepfunction-scenario-gen';
+import { ApiGateway } from './constructs/api-gateway';
+import { FrontendHosting } from './constructs/frontend-hosting';
 
 export class Chaos100Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,7 +13,18 @@ export class Chaos100Stack extends cdk.Stack {
       bucketName: `chaos-100-templates-${cdk.Aws.ACCOUNT_ID}-${cdk.Aws.REGION}`,
     });
 
-    // アウトプット
+    // API Gateway + UI Handler Lambda の作成
+    const apiGateway = new ApiGateway(this, 'ApiGateway', {
+      templateBucket: scenarioGenerator.templateBucket,
+      stateMachine: scenarioGenerator.stateMachine,
+    });
+
+    // フロントエンド静的ホスティングの作成
+    const frontendHosting = new FrontendHosting(this, 'FrontendHosting', {
+      apiGateway: apiGateway.api,
+    });
+
+    // 既存のアウトプット
     new cdk.CfnOutput(this, 'ScenarioGeneratorStateMachineArn', {
       value: scenarioGenerator.stateMachine.stateMachineArn,
       description: 'Scenario Generator State Machine ARN',
@@ -35,6 +48,22 @@ export class Chaos100Stack extends cdk.Stack {
     new cdk.CfnOutput(this, 'TemplateBucketName', {
       value: scenarioGenerator.templateBucket.bucketName,
       description: 'Template S3 Bucket Name',
+    });
+
+    // 新しいアウトプット
+    new cdk.CfnOutput(this, 'ApiGatewayUrl', {
+      value: apiGateway.api.url,
+      description: 'API Gateway URL',
+    });
+
+    new cdk.CfnOutput(this, 'UiHandlerLambdaArn', {
+      value: apiGateway.uiHandlerLambda.functionArn,
+      description: 'UI Handler Lambda Function ARN',
+    });
+
+    new cdk.CfnOutput(this, 'WebsiteUrl', {
+      value: `https://${frontendHosting.distribution.distributionDomainName}`,
+      description: 'Website URL',
     });
   }
 }
